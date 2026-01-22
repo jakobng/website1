73cff834b06b02d0ca232f91872cba47540b92b3
jakobng <75647257+jakobng@users.noreply.github.com>
2026-01-22 22:59:17 +0900

    Add files via upload

--- manchester_cinema_scrapers/cinema_modules/__init__.py
+++ manchester_cinema_scrapers/cinema_modules/__init__.py
@@ -1,2 +1,10 @@
-# Grok-Manchester/cinema_modules/__init__.py
-# Module package for Manchester cinema scrapers
+# Manchester Cinema Scrapers - Module Package
+
+from . import home_mcr_module
+from . import cultplex_module
+from . import savoy_module
+from . import everyman_manchester_module
+from . import regent_module
+from . import plaza_module
+from . import block_cinema_module
+from . import small_world_cinema_module
